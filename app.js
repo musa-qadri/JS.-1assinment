b9b923b396dc708710f041684b5742931c7d4fdf
musa al qadri <musaqazim1@gmail.com>
2023-10-10 22:55:12 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// chapter 1
+
+// Question 1
+alert("Error! please Enter a Valid Password")
+
+// Qusetion 2
+alert('Welcome to JS Land.. \n Happy coading!')
+
+// Question 3
+alert(' Welcome to JS Land..')
+
+// Question 4
+alert('Private this page from creating addational dialogs.')
+
+// Question 5
+alert("Hello.. I can run JS through my website browser's console")
